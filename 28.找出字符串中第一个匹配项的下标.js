59dbe12a24e5b72b583e7985d542d12835a6f63c
hezg <hzgdeemail@163.com>
2022-09-26 23:55:18 +0800

    NO.28 finished

---
+++ 28.找出字符串中第一个匹配项的下标.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=javascript
+ *
+ * [28] 找出字符串中第一个匹配项的下标
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  const match = haystack.match(needle)
+  console.log(match && match.index)
+  return match ? match.index : -1
+}
+
+strStr('leetcode', 'leeto')
+// @lc code=end
