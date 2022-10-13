25443f7b28744d703f1344cc696af69bcd9c9d02
hezg <hzgdeemail@163.com>
2022-10-13 23:53:31 +0800

    feat：add use two new linked  and p solve this problem.

---
+++ 86.分隔链表.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=javascript
+ *
+ * [86] 分隔链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+  // 小于x的链表
+  let dummyLow = new ListNode()
+  let p1 = dummyLow
+  // 大于等于x的链表
+  let dummyHigh = new ListNode()
+  let p2 = dummyHigh
+
+  while(head) {
+    let temp = head.next
+    if(head.val < x) {
+      p1.next = head
+      p1 = p1.next
+    } else {
+      p2.next = head
+      p2 = p2.next
+    }
+    head.next = null
+    head = temp
+  }
+  p1.next = dummyHigh.next
+  return dummyLow.next
+
+};
+// @lc code=end
+
